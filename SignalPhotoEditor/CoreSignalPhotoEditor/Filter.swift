9659df3e6faecd50a3bbc71f92d3329ac28e6d16
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2020-10-31 16:07:30 +0200

    Removed scene delegate

---
+++ SignalPhotoEditor/CoreSignalPhotoEditor/Filter.swift
@@ -0,0 +1,14 @@
+//
+//  Filter.swift
+//  SignalPhotoEditor
+//
+//  Created by Головаш Анастасия on 31.10.2020.
+//
+
+import Foundation
+
+struct Filter {
+    let filterName: String
+    var filterEffectValue: Any?
+    var filterEffectValueName: String?
+}
