8b02a7edd807ed855778212bdcddbe0d8720ef83
ddanilyuk <danis.danilyuk@gmail.com>
2020-10-31 16:26:31 +0200

    Add basic ui for testing filters

--- SignalPhotoEditor/SignalPhotoEditor/ViewController.swift
+++ SignalPhotoEditor/SignalPhotoEditor/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var applyButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupButtons()
     }
 
-
+    private func setupButtons() {
+        applyButton.layer.cornerRadius = 4
+        resetButton.layer.cornerRadius = 4
+    }
+    
+    @IBAction func didPressApply(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didPressReset(_ sender: UIButton) {
+        
+    }
+    
 }
-
