1d488b54d1f2cbff99b788903e14783b804d98d8
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2020-11-14 17:12:42 +0200

    LUTs creating added

---
+++ SignalPhotoEditor/SignalPhotoEditor/LUTsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LUTsViewController.swift
+//  SignalPhotoEditor
+//
+//  Created by Anastasia Holovash on 13.11.2020.
+//
+
+import UIKit
+
+class LUTsViewController: UIViewController {
+
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var image: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let image = image {
+            secondImageView.image = image
+        }
+    }
+    
+
+
+}
--- SignalPhotoEditor/SignalPhotoEditor/ViewController.swift
+++ SignalPhotoEditor/SignalPhotoEditor/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var applyButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var showLUTButton: UIButton!
+    @IBOutlet weak var useLUTButton: UIButton!
     
     private var originalImage: UIImage?
+    
+    private var filteres: [Filter] = []
+    
+    private var lutImage: UIImage?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +31,17 @@ class ViewController: UIViewController {
     private func setupButtons() {
         applyButton.layer.cornerRadius = 10
         resetButton.layer.cornerRadius = 10
+        showLUTButton.layer.cornerRadius = 10
+        useLUTButton.layer.cornerRadius = 10
     }
     
     @IBAction func didPressApply(_ sender: UIButton) {
         print("Apply pressed")
 
-        let filteres: [Filter] = [Filters.photoEffectFade.getFilter(intensity: 1),
+        filteres = [Filters.photoEffectFade.getFilter(intensity: 1),
                                   Filters.colorCube(lutImage: UIImage(named: "ClearLUT")!).getFilter(intensity: 1)]
         
-        let facade = Facade(image: mainImageView.image!, filteres: [filteres[1]])
+        let facade = Facade(image: mainImageView.image!, filteres: [filteres[0]])
         
         DispatchQueue.global().async {
             let image = facade.applyFilters()
@@ -47,4 +56,31 @@ class ViewController: UIViewController {
         mainImageView.image = originalImage
     }
     
+    @IBAction func didPressShowLUT(_ sender: UIButton) {
+        print("Show LUT pressed")
+        let facadeLUT = Facade(image: UIImage(named: "ClearLUT")!, filteres: [filteres[0]])
+//        var newLUTImage: UIImage
+        
+        DispatchQueue.global().async {
+            self.lutImage = facadeLUT.applyFilters()
+            DispatchQueue.main.async {
+                let lutsVC = self.storyboard?.instantiateViewController(withIdentifier: "LUTsViewController") as! LUTsViewController
+                lutsVC.image = self.lutImage
+                self.navigationController?.pushViewController(lutsVC, animated: true)
+                
+            }
+        }
+    }
+    
+    @IBAction func didPressUseLUTButton(_ sender: UIButton) {
+        print("Use LUT pressed")
+        let facade = Facade(image: mainImageView.image!, filteres: [Filters.colorCube(lutImage: lutImage ?? UIImage(named: "ClearLUT")!).getFilter(intensity: 1)])
+        
+        DispatchQueue.global().async {
+            let image = facade.applyFilters()
+            DispatchQueue.main.async {
+                self.mainImageView.image = image
+            }
+        }
+    }
 }
