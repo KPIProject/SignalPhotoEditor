c8814af417265d8a813db813f1f162ae7d40af77
ddanilyuk <danis.danilyuk@gmail.com>
2020-11-22 18:37:03 +0200

    Add Collection for filters

---
+++ SignalPhotoEditor/SignalPhotoEditor/FilterCollectionView/FilterModel.swift
@@ -0,0 +1,14 @@
+//
+//  FilterModel.swift
+//  SignalPhotoEditor
+//
+//  Created by Denys Danyliuk on 22.11.2020.
+//
+
+import UIKit
+
+struct FilterModel {
+    
+    var image: UIImage
+    var name: String
+}
--- SignalPhotoEditor/SignalPhotoEditor/MainViewController.swift
+++ SignalPhotoEditor/SignalPhotoEditor/MainViewController.swift
@@ -11,14 +11,31 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var contentView: UIView!
     
-
+    @IBOutlet weak var filterCollectionView: FilterCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.delegate = self
+
+        let filters: [FilterModel] = [
+            FilterModel(image: UIImage(named: "mountain")!, name: "Filter1"),
+            FilterModel(image: UIImage(named: "mountain")!, name: "Filter2"),
+            FilterModel(image: UIImage(named: "mountain")!, name: "Filter3"),
+            FilterModel(image: UIImage(named: "mountain")!, name: "Filter4"),
+            FilterModel(image: UIImage(named: "mountain")!, name: "Filter5"),
+            FilterModel(image: UIImage(named: "mountain")!, name: "Filter6")
+        ]
+        filterCollectionView.config(with: filters)
+        
+//        scrollView.contentInset = UIEdgeInsets(top: <#T##CGFloat#>, left: <#T##CGFloat#>, bottom: <#T##CGFloat#>, right: <#T##CGFloat#>)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
 
 }
 
@@ -26,6 +43,6 @@ class MainViewController: UIViewController {
 extension MainViewController: UIScrollViewDelegate {
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return  imageVIew
+        return contentView
     }
 }
