86f8671f66ff0cd99f771e2c413927054fbc4f70
ddanilyuk <danis.danilyuk@gmail.com>
2020-12-05 15:57:27 +0200

    Change border between tabBar and filter collection

--- SignalPhotoEditor/AppDelegate.swift
+++ SignalPhotoEditor/AppDelegate.swift
@@ -13,7 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        
         return true
     }
 
